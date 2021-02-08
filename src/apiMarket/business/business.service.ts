37a7df18c992b94b9b05e36d58165a571d7f799c
Oscar Gracia <oscar.graciae@gmail.com>
2021-02-08 11:48:05 -0600

    Cambio de busqueda de negocios de 5 a 10km

--- src/apiMarket/business/business.service.ts
+++ src/apiMarket/business/business.service.ts
@@ -22,7 +22,7 @@ export class BusinessService {
         .where(categoryId ? `business.businessCategoryId = :categoryId` : '1=1', { categoryId })
         .andWhere('business.isPublic = true')
         .innerJoinAndSelect('business.businessCategory', 'businessCategory')
-        .innerJoinAndSelect('business.businessAddress', 'businessAddress', '(6371 * acos(cos(radians(:lat)) * cos(radians(lat)) * cos(radians(:lng) - radians(lng)) + sin(radians(:lat)) * sin(radians(lat)))) <= 5', { lat, lng })
+        .innerJoinAndSelect('business.businessAddress', 'businessAddress', '(6371 * acos(cos(radians(:lat)) * cos(radians(lat)) * cos(radians(:lng) - radians(lng)) + sin(radians(:lat)) * sin(radians(lat)))) <= 10', { lat, lng })
         
         // .where("businessUser.userId = :userId", { userId: req.session.userId })
         // .where('(6371 * acos(cos(radians(:lat)) * cos(radians(lat)) * cos(radians(:lng) - radians(lng)) + sin(radians(:lng)) * sin(radians(lat)))) <= 10', { lat: 25.6866142, lng: -100.3161126 })
